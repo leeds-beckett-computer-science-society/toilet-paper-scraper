50c0239861f2fc9dc2adbccf79b1adc5eff4b429
Sam Lightowler <slightowler7@gmail.com>
2020-03-25 17:44:14 +0000

    scrape file

---
+++ gimme-tp.py
@@ -0,0 +1,8 @@
+#------------------------------------------------------------------------------------------
+# (C) 2020 Leeds Beckett Computer Science Society
+#
+#
+#
+# git: https://github.com/leeds-beckett-computer-science-society/toilet-paper-scraper.git
+#
+#------------------------------------------------------------------------------------------
