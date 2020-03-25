776d3cbc34774d0ad47a55e84b9c0569d834c3eb
Sam Lightowler <slightowler7@gmail.com>
2020-03-25 17:45:40 +0000

    quik fix

--- gimme-tp.py
+++ gimme-tp.py
@@ -1,6 +1,10 @@
 #------------------------------------------------------------------------------------------
 # (C) 2020 Leeds Beckett Computer Science Society
+# Authors:
 #
+#
+#
+# Descr: Web spider to search and alert available TP
 #
 #
 # git: https://github.com/leeds-beckett-computer-science-society/toilet-paper-scraper.git
