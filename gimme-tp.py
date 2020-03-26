db78b4ae2d2c97863e44b1a330f2c5d49d615bed
Sam Lightowler <slightowler7@gmail.com>
2020-03-26 13:01:25 +0000

    set up venv

--- gimme-tp.py
+++ gimme-tp.py
@@ -1,12 +1,26 @@
-#------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------
 # (C) 2020 Leeds Beckett Computer Science Society
 # Authors:
+# Bence Gadanyi
+# Lucy McCall
+# Samuel Lightowler
 #
-#
+# server link: https://discord.gg/RDmKHe
 #
 # Descr: Web spider to search and alert available TP
 #
 #
 # git: https://github.com/leeds-beckett-computer-science-society/toilet-paper-scraper.git
 #
-#------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------
+import sys
+
+print(sys.version)
+print(sys.executable)
+
+
+def test():
+    print("test")
+
+
+test()
