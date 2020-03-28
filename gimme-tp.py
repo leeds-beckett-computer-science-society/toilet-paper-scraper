aedccfc45e9149ec329fa89fdf870277eeacd11e
Sam Lightowler <slightowler7@gmail.com>
2020-03-28 20:34:35 +0000

    code refactor

--- gimme-tp.py
+++ gimme-tp.py
@@ -19,9 +19,6 @@ import scrapy
 import json
 import datetime
 import time
-import socks 
-import sockets 
-import stem.process
 from scrapy.crawler import CrawlerProcess
 from twilio.rest import Client
 from tpScraper.tpScraper.spiders.tpSpider import PaperSpider
@@ -32,40 +29,27 @@ def test_version_and_path():
     print(sys.executable)
    
 
-class PaperSpider(scrapy.Spider):
-    # anonymise scraping by using tor proxy
-    SOCKS_PORT=7000 
+# class PaperSpider(scrapy.Spider):
+    
 
-    tor_process = stem.process.launch_tor_with_config(
-        config = {
-            'SocksPort': str(SOCKS_PORT),
-        },
-    )
-    socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5,
-                          addr="127.0.0.1",
-                          port=SOCKS_PORT)
-    socket.socket = socks.socksocket
+    
+# #     name = 'toiletPaper'
 
-    #------------------------- scrapy script----------------------------#
+# #     start_urls = ['https://www.tesco.com/groceries/en-GB/search?query=toilet%20paper&icid=tescohp_sws-1_m-ft_in-toilet%20paper_ab-226-b_out-toilet%20paper']
 
-#     name = 'toiletPaper'
+# #     def parse(self, response):
+# #         print('TEST TEST TEST')
+# #         for product in response.xpath("//div[@class='product-list--list-item']"):
+# #             product_name = product.xpath(".//h3[@class='sc-kAzzGY iTLNpo']").extract_first()
+# #             product_price = product.xpath(".//span[@class='value']").extract_first()
+# #             print(product_name)
+# #             print(product_price)
+# #             # yield {
+# #             #     'product_name': product_name,
+# #             #     'product_price': product_price
+# #             # }
 
-#     start_urls = ['https://www.tesco.com/groceries/en-GB/search?query=toilet%20paper&icid=tescohp_sws-1_m-ft_in-toilet%20paper_ab-226-b_out-toilet%20paper']
-
-#     def parse(self, response):
-#         print('TEST TEST TEST')
-#         for product in response.xpath("//div[@class='product-list--list-item']"):
-#             product_name = product.xpath(".//h3[@class='sc-kAzzGY iTLNpo']").extract_first()
-#             product_price = product.xpath(".//span[@class='value']").extract_first()
-#             print(product_name)
-#             print(product_price)
-#             # yield {
-#             #     'product_name': product_name,
-#             #     'product_price': product_price
-#             # }
-
-    #------------------------- scrapy script----------------------------#
-    tor_process.kill()
+#     #------------------------- scrapy script----------------------------#
 
 
 class AlertModel:
@@ -106,10 +90,6 @@ class AlertModel:
 
         return data
             
-
-    def tor():
-        print('todo')
-
 
 #test_version_and_path()
 # AlertModel.alert_me()
--- venv/tpScraper/tpScraper/spiders/tpSpider.py
+++ venv/tpScraper/tpScraper/spiders/tpSpider.py
@@ -1,6 +1,23 @@
 import scrapy
+import socks 
+import sockets 
+import stem.process
 
 class PaperSpider(scrapy.Spider):
+    # anonymise scraping by using tor proxy
+    SOCKS_PORT=7000 
+
+    tor_process = stem.process.launch_tor_with_config(
+        config = {
+            'SocksPort': str(SOCKS_PORT),
+        },
+    )
+    socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5,
+                          addr="127.0.0.1",
+                          port=SOCKS_PORT)
+    socket.socket = socks.socksocket
+
+    #------------------------- scrapy script----------------------------#
     name = 'toiletPaper'
 
     start_urls = ['https://www.tesco.com/groceries/en-GB/search?query=toilet%20paper&icid=tescohp_sws-1_m-ft_in-toilet%20paper_ab-226-b_out-toilet%20paper']
@@ -22,3 +39,6 @@ class PaperSpider(scrapy.Spider):
             #     'product_url': product_url,
             #     'product_price': product_price
             # }
+
+    #------------------------- scrapy script----------------------------#
+    tor_process.kill()
